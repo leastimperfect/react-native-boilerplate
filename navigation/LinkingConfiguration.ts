a38fe30eb9c2f52afe5336f3f73a9838dd235a5b
Shramee Srivastav <shramee.srivastav@gmail.com>
2022-02-03 16:01:24 +0530

    Clear out linking config
    Add more stuff as we need.

--- navigation/LinkingConfiguration.ts
+++ navigation/LinkingConfiguration.ts
@@ -10,26 +10,10 @@ import * as Linking from 'expo-linking';
 import { RootStackParamList } from '../types';
 
 const linking: LinkingOptions<RootStackParamList> = {
-  prefixes: [Linking.makeUrl('/')],
+  prefixes: [Linking.createURL('/')],
+
   config: {
-    screens: {
-      Root: {
-        screens: {
-          TabOne: {
-            screens: {
-              TabOneScreen: 'one',
-            },
-          },
-          TabTwo: {
-            screens: {
-              TabTwoScreen: 'two',
-            },
-          },
-        },
-      },
-      Modal: 'modal',
-      NotFound: '*',
-    },
+    screens: {},
   },
 };
 

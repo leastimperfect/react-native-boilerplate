f0484d99b5c5cf091bad0d4e48a2ca5de1757d09
Shramee Srivastav <shramee.srivastav@gmail.com>
2022-02-04 15:00:05 +0530

    Use appstyles on NotFoundScreen

--- screens/NotFoundScreen.tsx
+++ screens/NotFoundScreen.tsx
@@ -2,10 +2,11 @@ import { StyleSheet, TouchableOpacity } from 'react-native';
 
 import { Text, View } from '../components/Themed';
 import { RootStackScreenProps } from '../types';
+import styles from '../appStyles';
 
 export default function NotFoundScreen({ navigation }: RootStackScreenProps<'NotFound'>) {
   return (
-    <View style={styles.container}>
+    <View style={[styles.wrapper, styles.hvCenter]}>
       <Text style={styles.title}>This screen doesn't exist.</Text>
       <TouchableOpacity onPress={() => navigation.replace('Root')} style={styles.link}>
         <Text style={styles.linkText}>Go to home screen!</Text>
@@ -13,24 +14,3 @@ export default function NotFoundScreen({ navigation }: RootStackScreenProps<'Not
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-  linkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
-});

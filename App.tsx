3d938e2c4b7ce734c4189d98f3b21fb0d62a4289
Shramee Srivastav <shramee.srivastav@gmail.com>
2022-02-05 13:02:30 +0530

    Refactored directories

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import {StatusBar} from 'expo-status-bar';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {Text, View} from 'react-native';
-import {AppStateManager} from './AppStateManager';
+import {AppStateManager} from './src/AppStateManager';
 
-import useCachedResources from './hooks/useCachedResources';
-import Navigation from './navigation';
+import useCachedResources from './src/hooks/useCachedResources';
+import Navigation from './src/navigation';
 
 export default function App() {
 	const isLoadingComplete = useCachedResources(); // Loads fonts

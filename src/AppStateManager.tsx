188044166e4e952e3436230ff0fdf0d0e0821374
Shramee Srivastav <shramee.srivastav@gmail.com>
2022-02-05 17:04:17 +0530

    Removing state manager
    For HOC state management

--- src/AppStateManager.tsx
+++ src/AppStateManager.tsx
@@ -6,15 +6,3 @@ const appStateContext = createContext( {
 
 export const AppProvider = appStateContext.Provider;
 export const AppConsumer = appStateContext.Consumer;
-
-export class AppStateManager {
-	private state: any;
-	private setState: (s: any) => void;
-
-	constructor( state: any, setState: (s: any) => void ) {
-		this.state = state;
-		this.setState = setState;
-	}
-
-
-}
